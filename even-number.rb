37febe0f0333f1c720d8fa52c85c1d3b6b27d45d
MayurM-weboniselab <mayur.mahajan@weboniselab.com>
2023-02-27 15:34:31 +0530

    feature-even-number | finding count of even numbers from an array e# Please enter the commit message for your changes. Lines starting

--- even-number.rb
+++ even-number.rb
@@ -1,20 +1,24 @@
 # #by using block
 array = [1, 3, 5, 7, 9, 33, 55, 98, 61, 22]
+cnt1 = 0
 array.each do |no|
-  puts "#{no} is a even number" if no.even?
+  cnt1 += 1 if no.even?
 end
+puts cnt1
 
 # by using lambda expression
 even_number_1 = lambda { |no|
   no.even?
 }
 even_number_array = array.select(& even_number_1)
-puts even_number_array
+puts even_number_array.count
 
 # by using procs
 prime_no_2 = proc do |arr|
+  cnt2 = 0
   arr.each do |no|
-    puts "#{no} is a even number" if no.even?
+    cnt2 += 1 if no.even?
   end
+  puts cnt2
 end
 prime_no_2.call(array)
